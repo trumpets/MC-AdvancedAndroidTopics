7d36bd729a89d473be28916de834437cc38b15ea
Ivo Neskovic <ivo.neskovic@gmail.com>
2019-05-13 10:44:41 +0200

    Updating build scripts to latest android + studio + gradle versions.

--- app/src/main/java/gr/academic/city/mc/studentmanagement/MainActivity.java
+++ app/src/main/java/gr/academic/city/mc/studentmanagement/MainActivity.java
@@ -2,7 +2,7 @@ package gr.academic.city.mc.studentmanagement;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
--- app/src/main/java/gr/academic/city/mc/studentmanagement/StudentDetailsActivity.java
+++ app/src/main/java/gr/academic/city/mc/studentmanagement/StudentDetailsActivity.java
@@ -3,8 +3,8 @@ package gr.academic.city.mc.studentmanagement;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 /**
